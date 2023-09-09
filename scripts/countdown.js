017c0c2dd6dbd546fe604d2bcdd9f7bb36e96cb4
Jordan Lee <jtlee@rmgj2.com>
2023-09-09 01:09:14 -0500

    Added countdown to mobile view

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,6 +1,8 @@
 function countDown(){
     // Variable Declarations
-    const countDownDisplay = document.querySelector("#countdown");
+    const countDownDisplay = document.getElementsByClassName("count-down-counter");
+    // goes through the array with the class name count-down-counter and creates the create down for each element in the array
+
     let countDownDate = new Date("Feb 3, 2024 00:00:00").getTime();
     let present = new Date().getTime();
     let distance = countDownDate - present;
@@ -10,10 +12,10 @@ function countDown(){
     let minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
     let seconds = Math.floor((distance % (1000*60)) / 1000);
 
-
     // Displaying the Countdown
-    countDownDisplay.innerHTML = `<p>${months}m ${days}d ${hours}h ${minutes}m ${seconds}s</p>`;
-    countDown.className = "countdown";
+    for (let i = 0; i < countDownDisplay.length; i++) {
+        countDownDisplay[i].innerHTML = `<p>${months}m ${days}d ${hours}h ${minutes}m ${seconds}s</p>`;
+    }
 
     // Changing text for countdown ending/event starting
     if (distance < 0) {
