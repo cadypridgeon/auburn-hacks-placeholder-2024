75600db053631d571d75ecd388467fab3a1358b7
Matthew Rogers <rogersmatt350@gmail.com>
2023-09-24 18:41:39 -0500

    fix mobile nav blocking content

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -15,19 +15,21 @@ window.addEventListener("scroll", function () {
 });
 
 const menuCheckbox = document.querySelector("#menu-checkbox");
-const menu = document.querySelector("#menu")
+const menu = document.querySelector("#menu");
 const menuToggle = document.querySelector("#menu-toggle");
 
 menuCheckbox.addEventListener("change", function () {
- if (this.checked) {
-  setTimeout(() => {
-  menu.style.display = "flex";
-  menuToggle.style.width = "100vw";
-  }, 0);
- } else {
-  setTimeout(() => {
-  menu.style.display = "none";
-  menuToggle.style.width = "0";
-  }, 500);
- }
-})
+  if (this.checked) {
+    setTimeout(() => {
+      menu.style.display = "flex";
+      menuToggle.style.width = "100vw";
+      menuToggle.style.height = "100vh";
+    }, 0);
+  } else {
+    setTimeout(() => {
+      menu.style.display = "none";
+      menuToggle.style.width = "0";
+      menuToggle.style.height = "100%";
+    }, 500);
+  }
+});
